2946c097fd8429ca12015b8ebb3b001f26de46ef
Mattia Astorino <astorino.mattia@gmail.com>
2019-07-19 18:03:40 +0200

    Add dist

---
+++ dist/rules.d.ts
@@ -0,0 +1,17 @@
+import { IColorSet } from "./interfaces";
+export interface IVscodeJsonThemeSetting {
+    name: string;
+    scope: string | string[];
+    settings: {
+        foreground?: string;
+        fontStyle?: string;
+    };
+}
+export declare type ColorFetcher = (colorSet: IColorSet) => string;
+export declare type ColorGenerator = (color: string) => any;
+export interface IRuleGenerator {
+    color: ColorFetcher;
+    generate: ColorGenerator;
+}
+export declare const globalRules: IRuleGenerator[];
+export declare const tokenRules: IRuleGenerator[];
