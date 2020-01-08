8d233416cc0bb27eb5895c4797fe6b77abe560db
Mattia Astorino <astorino.mattia@gmail.com>
2020-01-08 10:05:12 +0100

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,17 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var vscodeThemeGenerator_1 = require("./vscodeThemeGenerator");
+const fs = __importStar(require("fs"));
+const vscodeThemeGenerator_1 = require("./vscodeThemeGenerator");
 function generateTheme(themeName, colorSet, outputFile) {
-    var themeJson = new vscodeThemeGenerator_1.VscodeThemeGenerator().generateTheme(themeName, colorSet);
+    const themeJson = new vscodeThemeGenerator_1.VscodeThemeGenerator().generateTheme(themeName, colorSet);
     fs.writeFileSync(outputFile, themeJson);
 }
 exports.generateTheme = generateTheme;
+//# sourceMappingURL=index.js.map
