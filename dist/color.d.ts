9e43b96656dc9ed1fa7c7c6e98b85c9fe6c6cb25
Mattia Astorino <astorino.mattia@gmail.com>
2020-01-09 19:19:16 +0100

    Remove dist

--- dist/color.d.ts
+++
@@ -1,6 +0,0 @@
-import { IColorSet, IBaseColorSet } from './interfaces';
-export declare function lighten(color: string, amount: number): string;
-export declare function darken(color: string, amount: number): string;
-export declare function addAlpha(color: string, alpha: number): string;
-export declare function contast(color: string): string;
-export declare function generateFallbackColorSet(s: IBaseColorSet, type: 'light' | 'dark'): IColorSet;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { IColorSet } from './interfaces';
-export declare function generateTheme(themeName: string, colorSet: IColorSet, outputFile: string): void;
-export { IColorSet } from './interfaces';
